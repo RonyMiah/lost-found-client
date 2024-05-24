8eebeca3100822b02658f0f1f56cac6bcae22240
Rony <rony@Ronys-Air.lan>
2024-05-24 20:23:12 +0600

    redux setup done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
 import { Toaster } from 'sonner';
-
+import Providers from '@/lib/Providers/Providers';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
@@ -18,15 +18,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <AppRouterCacheProvider>
-          <>
-            <Toaster position="top-center" />
-            {children}
-          </>
-        </AppRouterCacheProvider>
-      </body>
-    </html>
+    <Providers>
+      <html lang="en">
+        <body className={inter.className}>
+          <AppRouterCacheProvider>
+            <>
+              <Toaster position="top-center" />
+              {children}
+            </>
+          </AppRouterCacheProvider>
+        </body>
+      </html>
+    </Providers>
   );
 }
---
+++ src/redux/tag-types.ts
@@ -0,0 +1,4 @@
+export enum tagTypes {
+  admin = 'admin',
+  user = 'user',
+}
